e12e02097a9b0384f782cd8cbfe3f3528439525b
vinhqd <vinhqd1996@gmail.com>
2020-07-17 16:49:28 +0700

    register user

--- index.js
+++ index.js
@@ -8,8 +8,25 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 server.listen(3000, () => console.log('Listening port 3000.'));
 
+var users = [];
+
 io.on('connection', (socket) => {
-    console.log('Co nguoi ket noi: ' + socket.id)
+    console.log('Co nguoi ket noi: ' + socket.id);
+    socket.on('client-send-username', data => {
+        if (users.indexOf(data) >= 0) {
+            socket.emit('server-send-fail');
+        } else {
+            users.push(data);
+            socket.userName = data;
+            socket.emit('server-send-success', data);
+            io.sockets.emit('server-send-users', users);
+        }
+    })
+
+    socket.on('logout', () => {
+        users.splice(users.indexOf(socket.userName), 1);
+        socket.broadcast.emit('server-send-users', users)
+    })
 });
 
 app.get('/', (req, res) => {
--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,38 @@
 const socket = io('http://localhost:3000/');
 
+socket.on('server-send-fail', () => {
+    alert('Co nguoi da dang ky roi!!!');
+})
+
+socket.on('server-send-success', (data) => {
+    $('#loginForm').hide(2000);
+    $('#chatForm').show(1000);
+    $('#currentUser').html(data)
+});
+
+socket.on('server-send-users', users => {
+    $('#boxContent').html("");
+    users.forEach(user => {
+        // useronline
+        $('#boxContent').append(`<div class='useronline'>${user}</div>`)
+    });
+})
+
 $(document).ready(() => {
     $('#loginForm').show();
     $('#chatForm').hide();
+
+    $('#btnRegister').click(() => {
+        socket.emit('client-send-username', $('#txtUsername').val());
+    });
+
+    $('#btnLogout').click(() => {
+        socket.emit('logout');
+        $('#chatForm').hide(2000);
+        $('#loginForm').show(1000);
+    });
+
+    $("#btnSendMessage").click(() => {
+        
+    })
 });
